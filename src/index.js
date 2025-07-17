be73fa6737bf5f02ac4f58079dc1d98fb18a3ca8
h5avlov <h5avlov@abv.bg>
2025-07-17 21:33:06 +0300

    Unnecesssary imports removed from 'index'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const express = require('express'); 
-const handlebars = require('express-handlebars'); 
-const mongoose = require('mongoose'); 
 const expr = require('./config/express.config');
 const hbrs = require('./config/handlebars.config');
 const mngs = require('./config/mongoose.config');
