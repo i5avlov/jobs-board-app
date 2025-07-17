802967ca502a377077d60db24c71ec57ca6e0e2e
h5avlov <h5avlov@abv.bg>
2025-07-17 21:14:55 +0300

    Installed Mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express'); 
 const handlebars = require('express-handlebars'); 
+const mongoose = require('mongoose'); 
 
 const app = express(); 
 
 app.engine('hbs', handlebars.engine({ extname: 'hbs' })); 
 app.set('view engine', 'hbs'); 
 app.set('views', 'src/views'); 
+
+mongoose
+    .connect('mongodb://localhost:27017', { dbName: 'jobs_board_app' }) 
+    .then(() => console.log('Connected to DB.')) 
+    .catch((err) => console.log('Error connecting to DB:', err.message)); 
 
 app.get('/', (req, res) => {
     res.render('index'); 
