60fe4ee44c87720572bc52780f6ac1d70740f193
h5avlov <h5avlov@abv.bg>
2025-07-17 21:09:19 +0300

    Installed Handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express'); 
+const handlebars = require('express-handlebars'); 
 
 const app = express(); 
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' })); 
+app.set('view engine', 'hbs'); 
+app.set('views', 'src/views'); 
+
 app.get('/', (req, res) => {
-    res.send('Works.'); 
+    res.render('index'); 
 }); 
 
 app.listen(4000, () => console.log('Server listening...')); 
