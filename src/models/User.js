dc16d85806b36bca90a52af2fdf6537dc0c89555
h5avlov <h5avlov@abv.bg>
2025-07-18 09:52:38 +0300

    'User' model added

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose'); 
+const bcrypt = require('bcrypt'); 
+const { PASSWORD } = require('../constants/security');
+
+const userSchema = new Schema({ 
+    username: { 
+        type: String, 
+        required: [true, 'Username is required'] 
+    }, 
+    email: {
+        type: String, 
+        required: [true, 'Email is required']
+    }, 
+    password: {
+        type: String, 
+        required: [true, 'Password is required'] 
+    } 
+}); 
+
+userSchema.pre('save', async function() { 
+    this.password = await bcrypt.hash(this.password, PASSWORD.HASH_ROUNDS); 
+}); 
+
+const User = model('User', userSchema); 
+
+module.exports = User; 
