a64d5b1c352eb7b1d992d866a4e53a7c019e405f
h5avlov <h5avlov@abv.bg>
2025-07-18 09:55:08 +0300

    Error message constants added

---
+++ src/constants/error.messages.js
@@ -0,0 +1,22 @@
+module.exports = { 
+    ERROR_MESSAGES: { 
+        
+        REQUIRED: (field) => `${capitalize(field)} is required`, 
+        MIN_LENGTH: (field, value) => `${capitalize(field)} has to be minimum ${value} characters long`, 
+        MAX_LENGTH: (field, value) => `${capitalize(field)} has to be maximum ${value} characters long`, 
+
+        EMAIL: { 
+            NOT_AN_EMAIL: 'The email is not valid' 
+        }, 
+
+        PASSWORD: {
+            NO_LOWERCASE_CHAR, 
+            NO_UPPERCASE_CHAR, 
+            NO_DIGIT
+        }
+    }
+}; 
+
+function capitalize(str = '') { 
+    return str.charAt(0).toUpperCase() + str.slice(1); 
+}
