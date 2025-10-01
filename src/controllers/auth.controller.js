b6e1c54dd446de28762cb10ecf65f83545c5d72c
h5avlov <h5avlov@abv.bg>
2025-10-01 16:00:52 +0300

    Base login validation with Express validator set up

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const authController = require('express').Router();
 const authService = require('../services/auth.service');
 const errorUtils = require('../utils/error.utils'); 
 const registerValidation = require('../validation/register.validation'); 
+const loginValidation = require('../validation/login.validation'); 
 
 authController
     .get('/register', (req, res) => {
@@ -34,12 +35,22 @@ authController
     .get('/login', (req, res) => {
         res.render('auth/login'); 
     }) 
-    .post('/login', async (req, res) => { 
+    .post('/login', loginValidation.runValidations(), async (req, res) => { 
+        // Get validation result 
+        const validationErrors = loginValidation.getValidationResult(req); 
+        // Get login data 
         const loginData = req.body; 
 
         try { 
+            if (false === validationErrors.isEmpty()) { 
+                throw validationErrors; 
+            } 
+
+            // Generate token if login is successful 
             const authToken = await authService.login(loginData); 
+            // Set token in cookie  
             res.cookie('user', authToken); 
+            
             res.redirect('/'); 
         } catch (err) { 
             res.render('auth/login', { loginData, errors: errorUtils.normalize(err) }); 
